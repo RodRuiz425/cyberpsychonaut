de0f31510d62816af5909823153d6a039dbe800d
Rodrigo Ruiz <rodrigoruiz425@gmail.com>
2023-03-08 11:17:32 -0600

    Change name and icon

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Cyber Psychonaut 111
+        </h1>
+        <p>
+        “El deseo de alterar periódicamente la conciencia es un impulso innato, natural, análogo al hambre o al impulso sexual.” 
+        </p>
+        <p>
+        –Dr. Andrew T. The Natural Mind. 
         </p>
       </header>
     </div>
