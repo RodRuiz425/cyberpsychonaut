8a92d4293ecd6e297787df1d4ee1c2cdf217245b
Rodrigo Ruiz <rodrigoruiz425@gmail.com>
2023-03-07 23:13:04 -0600

    commit fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Cyber Psychonaut1
+          Cyber Psychonaut111
         </p>
         <a
           className="App-link"
