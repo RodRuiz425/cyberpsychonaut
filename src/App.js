df0fee52a778ea7f13625a2caa9c6275613f8fa3
Rodrigo Ruiz <rodrigoruiz425@gmail.com>
2023-03-08 01:02:07 -0600

    fix background and name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import loading from './img/loading.png'
+import logo from './img/mandala.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={loading} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Cyber Psychonaut 111
         </p>
