014ab36d74f264640899b46bab29e1afd9b6a802
Rodrigo Ruiz <rodrigoruiz425@gmail.com>
2023-05-21 00:28:57 -0600

    Galeria y navegacion

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logotipo from './img/loading.png';
 import logo from './img/logo.png';
+import GalleryComponent from './components/galleryComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logotipo} className="App-logotipo" alt="logotipo" />
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className='Navigation'>
+          <ul>
+            <li className='logotipo'>
+              <img src={logotipo} className="App-logotipo" alt="logotipo" />
+            </li>
+            <li className='logo'>
+              <img src={logo} className="App-logo" alt="logo" />
+            </li>
+          </ul>
+        </div>
+        <div className='GalleryContainer'>
+          <GalleryComponent className="Gallery"/>
+        </div>
       </header>
     </div>
   );
