302623083da944709b109ab52fd1ca2fcdcfdd35
Rodrigo Ruiz <rodrigoruiz425@gmail.com>
2023-06-26 14:54:16 -0600

    acerca de

--- src/components/AboutComponent.jsx
+++ src/components/AboutComponent.jsx
@@ -10,11 +10,13 @@ const AboutComponent = () => {
           </div>
           <div className='aboutText'>
               <h4 className='aboutTitle'>
-                  SOBRE CYBER-PSYCHONAUT.COM
+                  Acerca de CYBER-PSYCHONAUT.COM
               </h4>
               <hr/>
               <p>
-                  Este es un sitio dedicado a la alimentación del Inconsciente Colectivo mediante la guerra memetica.
+              Este es un portal dedicado a la alimentación del Inconsciente Colectivo a través de la aplicación de la guerra memética y la difusión de contenidos de 
+              interés psiconautico. A través de la exposición de estos contenidos se busca ampliar los horizontes de la comprensión colectiva, proporcionando una mayor 
+              introspección y reflexión sobre las practicas psiconáuticas y mágico-religiosas.
               </p>
           </div>
       </div>
